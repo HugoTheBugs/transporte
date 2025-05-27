9bda2566695d611c6e096ee20a18dc03902d9837
Hugo Ferreira <hugoferreira2323@gmail.com>
2025-05-26 23:23:15 -0300

    feat: cria o mapper no application

--- src/main/java/edu/fema/transporte/TransporteApplication.java
+++ src/main/java/edu/fema/transporte/TransporteApplication.java
@@ -1,10 +1,17 @@
 package edu.fema.transporte;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TransporteApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransporteApplication.class, args);
