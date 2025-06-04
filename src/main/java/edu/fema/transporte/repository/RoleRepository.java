13c1f1653347b28c9a235292380b21c3e53d4fb0
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-06-04 11:21:43 -0300

    fix: remoção do security

--- src/main/java/edu/fema/transporte/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.fema.transporte.repository;
-
-import edu.fema.transporte.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
